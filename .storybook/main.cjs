ee051a55cc525faaeca22945723ef9bbb1f06970
Håvard Holvik <havard@whereby.com>
2022-12-16 13:43:28 +0100

    Convert project to TypeScript
    In preparation of a lower-level api, this change converts the existing
    project to Typescript. No functionality changes, and existing routines
    should work as normal.
    
    A few minor TODOS before this can be merged tho:
    - Clean up tsconfig.json to remove unnecessary config
    - Make sure addEventListener("message", <listener>) actually works
    - Clean up Heresy type definitions

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,5 +1,11 @@
 module.exports = {
-    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-    framework: "@storybook/web-components",
-};
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/web-components"
+}
--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
+import typescript from "rollup-plugin-typescript2";
 
 const replaceValues = {
     preventAssignment: true,
@@ -13,34 +14,34 @@ const replaceValues = {
 export default [
     // Commonjs build of lib, to be used with bundlers
     {
-        input: "src/lib/index.js",
+        input: "src/lib/index.ts",
         output: {
             exports: "default",
             file: "dist/lib.cjs.js",
             format: "cjs",
         },
         external: ["heresy"],
-        plugins: [replace(replaceValues)],
+        plugins: [replace(replaceValues), typescript()],
     },
     // Esm build of lib, to be used with bundlers
     {
-        input: "src/lib/index.js",
+        input: "src/lib/index.ts",
         output: {
             exports: "default",
             file: "dist/lib.esm.js",
             format: "esm",
         },
         external: ["heresy"],
-        plugins: [replace(replaceValues)],
+        plugins: [replace(replaceValues), typescript()],
     },
     // Legacy build of lib in ES format, bundling the dependencies
     {
-        input: "src/lib/index.js",
+        input: "src/lib/index.ts",
         output: {
             exports: "default",
             file: `dist/v${pkg.version.split(".")[0]}.js`,
             format: "esm",
         },
-        plugins: [nodeResolve(), terser(), replace(replaceValues)],
+        plugins: [nodeResolve(), terser(), replace(replaceValues), typescript()],
     },
 ];
