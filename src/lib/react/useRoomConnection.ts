d49f77e65524c6e4c0ec80d86400d509952b34fa
Håvard Holvik <havard@whereby.com>
2022-12-29 11:56:02 +0100

    Add support for actions

--- src/lib/react/useRoomConnection.ts
+++ src/lib/react/useRoomConnection.ts
@@ -1,99 +1,23 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import RoomConnection, { RoomConnectionOptions } from "../RoomConnection";
-import { LocalParticipant, RemoteParticipant } from "../RoomParticipant";
+import reducer, { RoomState } from "../reducer";
 
-interface RoomState {
-    localParticipant?: LocalParticipant;
-    roomConnectionStatus?: "connecting" | "connected" | "disconnected";
-    remoteParticipants: RemoteParticipant[];
+interface RoomConnectionActions {
+    toggleCamera(enabled?: boolean): void;
+    toggleMicrophone(enabled?: boolean): void;
 }
 
-type Action =
-    | {
-          type: "ROOM_JOINED";
-          payload: {
-              localParticipant: LocalParticipant;
-              remoteParticipants: RemoteParticipant[];
-          };
-      }
-    | {
-          type: "PARTICIPANT_JOINED";
-          payload: {
-              paritipant: RemoteParticipant;
-          };
-      }
-    | {
-          type: "PARTICIPANT_LEFT";
-          payload: {
-              participantId: string;
-          };
-      }
-    | {
-          type: "PARTICIPANT_STREAM_ADDED";
-          payload: {
-              participantId: string;
-              stream: MediaStream;
-          };
-      };
-
-function updateParticipant(
-    remoteParticipants: RemoteParticipant[],
-    participantId: string,
-    updates: Partial<RemoteParticipant>
-): RemoteParticipant[] {
-    const existingParticipant = remoteParticipants.find((p) => p.id === participantId);
-    if (!existingParticipant) {
-        return remoteParticipants;
-    }
-    const index = remoteParticipants.indexOf(existingParticipant);
-
-    return [
-        ...remoteParticipants.slice(0, index),
-        { ...existingParticipant, ...updates },
-        ...remoteParticipants.slice(index + 1),
-    ];
-}
-
-function reducer(state: RoomState, action: Action): RoomState {
-    switch (action.type) {
-        case "ROOM_JOINED":
-            return {
-                ...state,
-                localParticipant: action.payload.localParticipant,
-                remoteParticipants: action.payload.remoteParticipants,
-                roomConnectionStatus: "connected",
-            };
-        case "PARTICIPANT_JOINED":
-            return {
-                ...state,
-                remoteParticipants: [...state.remoteParticipants, action.payload.paritipant],
-            };
-        case "PARTICIPANT_LEFT":
-            return {
-                ...state,
-                remoteParticipants: [...state.remoteParticipants.filter((p) => p.id !== action.payload.participantId)],
-            };
-        case "PARTICIPANT_STREAM_ADDED":
-            return {
-                ...state,
-                remoteParticipants: updateParticipant(state.remoteParticipants, action.payload.participantId, {
-                    stream: action.payload.stream,
-                }),
-            };
-        default:
-            throw state;
-    }
-}
-
-export default function useRoomConnection(roomUrl: string, roomConnectionOptions: RoomConnectionOptions) {
+export default function useRoomConnection(
+    roomUrl: string,
+    roomConnectionOptions: RoomConnectionOptions
+): [state: RoomState, actions: RoomConnectionActions] {
+    const [roomConnection] = useState(new RoomConnection(roomUrl, roomConnectionOptions));
     const [state, dispatch] = useReducer(reducer, { remoteParticipants: [] });
 
-    const roomConnection = new RoomConnection(roomUrl, roomConnectionOptions);
-
     useEffect(() => {
-        roomConnection.addEventListener("room_joined", (e) => {
-            const { localParticipant, remoteParticipants } = e.detail;
-            dispatch({ type: "ROOM_JOINED", payload: { localParticipant, remoteParticipants } });
+        roomConnection.addEventListener("participant_audio_enabled", (e) => {
+            const { participantId, isAudioEnabled } = e.detail;
+            dispatch({ type: "PARTICIPANT_AUDIO_ENABLED", payload: { participantId, isAudioEnabled } });
         });
 
         roomConnection.addEventListener("participant_joined", (e) => {
@@ -111,6 +35,16 @@ export default function useRoomConnection(roomUrl: string, roomConnectionOptions
             dispatch({ type: "PARTICIPANT_STREAM_ADDED", payload: { participantId, stream } });
         });
 
+        roomConnection.addEventListener("room_joined", (e) => {
+            const { localParticipant, remoteParticipants } = e.detail;
+            dispatch({ type: "ROOM_JOINED", payload: { localParticipant, remoteParticipants } });
+        });
+
+        roomConnection.addEventListener("participant_video_enabled", (e) => {
+            const { participantId, isVideoEnabled } = e.detail;
+            dispatch({ type: "PARTICIPANT_VIDEO_ENABLED", payload: { participantId, isVideoEnabled } });
+        });
+
         roomConnection.join();
 
         return () => {
@@ -118,5 +52,15 @@ export default function useRoomConnection(roomUrl: string, roomConnectionOptions
         };
     }, [roomUrl]);
 
-    return [state];
+    return [
+        state,
+        {
+            toggleCamera: (enabled) => {
+                roomConnection.toggleCamera(enabled);
+            },
+            toggleMicrophone: (enabled) => {
+                roomConnection.toggleMicrophone(enabled);
+            },
+        },
+    ];
 }
