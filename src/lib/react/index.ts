b72095ceab939f7e2710fedef4eadba250c02475
Håvard Holvik <havard@whereby.com>
2023-01-11 15:39:26 +0100

    useLocalMedia hook

--- src/lib/react/index.ts
+++ src/lib/react/index.ts
@@ -1,2 +1,3 @@
 export { default as VideoElement } from "./VideoElement";
+export { default as useLocalMedia } from "./useLocalMedia";
 export { default as useRoomConnection } from "./useRoomConnection";
