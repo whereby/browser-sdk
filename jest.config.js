d64159c62957ba176e95a66498f5c305ed7cdb00
Håvard Holvik <havard@whereby.com>
2023-02-03 14:30:00 +0100

    Only look for test files in src folder

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 export default {
     preset: "ts-jest",
     testEnvironment: "jest-environment-node",
+    testMatch: ["<rootDir>/src/**/__tests__/**/*.[jt]s?(x)", "<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)"],
     transform: {},
 };
