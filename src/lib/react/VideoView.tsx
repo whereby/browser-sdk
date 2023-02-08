6555ad96d6ad4678ada10598c154251a84183db6
Håvard Holvik <havard@whereby.com>
2023-02-08 10:44:22 +0100

    Handle video muted attribute programmatically
    It seems passing muted attribute to video element has some gotchas
    depending on autoplay policy and readystate of the stream. This change
    here makes us handle the muted attribute manually, like setting the
    srcObject stream.

--- src/lib/react/VideoView.tsx
+++ src/lib/react/VideoView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useEffect, useRef } from "react";
 
 interface VideoViewSelfProps {
     stream: MediaStream;
@@ -8,12 +8,24 @@ interface VideoViewSelfProps {
 type VideoViewProps = VideoViewSelfProps &
     React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>;
 
-export default ({ stream, ...rest }: VideoViewProps) => {
-    const videoEl = useCallback<(node: HTMLVideoElement) => void>((node) => {
-        if (node !== null && node.srcObject !== stream) {
-            node.srcObject = stream;
+export default ({ muted, stream, ...rest }: VideoViewProps) => {
+    const videoEl = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (!videoEl.current) {
+            return;
         }
-    }, []);
+
+        if (videoEl.current.srcObject !== stream) {
+            videoEl.current.srcObject = stream;
+        }
+
+        // Handle muting programatically, not as video attribute
+        // https://stackoverflow.com/questions/14111917/html5-video-muted-but-still-playing
+        if (videoEl.current.muted !== muted) {
+            videoEl.current.muted = Boolean(muted);
+        }
+    }, [muted, stream, videoEl]);
 
     return <video ref={videoEl} autoPlay playsInline {...rest} />;
 };
