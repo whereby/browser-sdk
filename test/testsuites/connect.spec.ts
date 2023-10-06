1bfdf95a6c84ff3da58d40a47ba4b40413657eda
Nandor Biro <nandor@whereby.com>
2023-10-06 16:47:25 +0200

    Use transient rooms for api testing
    * Create test utils to create and delete transient rooms.
    * Refactor connect room test to use transient room for testing.

--- test/testsuites/connect.spec.ts
+++ test/testsuites/connect.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect } from "@playwright/test";
+import { createTransientRoom, deleteTransientRoom, joinRoom } from "./utils/room";
 
-test("connect room", async ({ page }) => {
-    const roomUrl = process.env.REACT_APP_SMALL_ROOM_HOST_URL;
-    if (!roomUrl) {
-        throw new Error("REACT_APP_SMALL_ROOM_HOST_URL not set");
-    }
-    await page.goto("http://127.0.0.1:3001");
-    const locator = page.locator("h1");
-    await expect(locator).toContainText(/Enter room URL/);
+test.describe("unlocked room", () => {
+    let meetingId: string;
+    let roomUrl: string;
 
-    const input = page.locator("input");
-    await input.fill(roomUrl);
-    await page.click("button");
+    test.beforeEach(async () => {
+        ({ meetingId, roomUrl } = await createTransientRoom({
+            isLocked: false,
+            roomMode: "normal",
+        }));
+    });
 
-    await expect(page.locator("h1")).toContainText(/Room/);
-    await expect(page.locator("dd[data-testid='roomConnectionStatus']")).toContainText("connected");
+    test.afterEach(async () => {
+        await deleteTransientRoom(meetingId);
+    });
+
+    test("join room", async ({ page }) => {
+        await joinRoom(page, roomUrl);
+
+        await expect(page.locator("h1")).toContainText(/Room/);
+        await expect(page.locator("dd[data-testid='roomConnectionStatus']")).toContainText("connected");
+    });
 });
