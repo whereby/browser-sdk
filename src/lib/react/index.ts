ac4a6843e1be7b7c4b920f6b67b6c78a58e936de
Kevin <138459303+kevinwhereby@users.noreply.github.com>
2023-10-12 22:34:53 +0100

    Revert "Move to esbuild"

--- src/lib/react/index.ts
+++ src/lib/react/index.ts
@@ -1,3 +1,3 @@
 export { default as VideoView } from "./VideoView";
 export { default as useLocalMedia } from "./useLocalMedia";
-export { useRoomConnection } from "./useRoomConnection";
+export { default as useRoomConnection } from "./useRoomConnection";
--- src/stories/components/VideoExperience.tsx
+++ src/stories/components/VideoExperience.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import DisplayNameForm from "./DisplayNameForm";
 import { LocalMediaRef } from "../../lib/react/useLocalMedia";
-import { useRoomConnection } from "../../lib/react/useRoomConnection";
+import useRoomConnection from "../../lib/react/useRoomConnection";
 
 export default function VideoExperience({
     displayName,
