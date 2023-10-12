79209df08c33ab7bab593fa60ec8f91a2a6bcafa
Nandor Biro <nandor@whereby.com>
2023-10-12 13:18:56 +0200

    Test if video is not frozen
    Collect samples by counting the frames and make some stats. Check if the
    minimun fps is greater than 1, so the video is not frozen.
    
    This test cannot run in WebKit, as it does not provide any API to count
    frames.

--- test/testsuites/video.spec.ts
+++ test/testsuites/video.spec.ts
@@ -1,6 +1,11 @@
 import { test, expect } from "@playwright/test";
 import { createTransientRoom, deleteTransientRoom, joinRoom, RoomMode } from "./utils/room";
-import { getAudioTrackPropertiesFromHTMLVideoElement } from "./utils/media";
+import {
+    countFrames,
+    CountFramesFunction,
+    getAudioTrackPropertiesFromHTMLVideoElement,
+    makeFrameStatistics,
+} from "./utils/media";
 
 const roomModes: RoomMode[] = ["normal", "group"];
 
@@ -45,6 +50,32 @@ roomModes.forEach((roomMode) => {
             expect(participant2AudioTrackProperties).toHaveProperty("muted", false);
             expect(participant2AudioTrackProperties).toHaveProperty("enabled", true);
             await expect(participant2.getByTestId("remoteParticipant")).toHaveJSProperty("muted", false);
+        });
+
+        test("video is not frozen", async ({ page, browserName }) => {
+            test.skip(browserName === "webkit", "Webkit does not have an API to get video frame stats.");
+
+            const participant1 = page;
+            await joinRoom({ page, roomUrl });
+
+            const participant2 = await page.context().newPage();
+            await joinRoom({ page: participant2, roomUrl });
+
+            // 4 = HAVE_ENOUGH_DATA - It tells that there is enough data available to start playing.
+            await expect(participant1.getByTestId("remoteParticipant")).toHaveJSProperty("readyState", 4);
+            await expect(participant2.getByTestId("remoteParticipant")).toHaveJSProperty("readyState", 4);
+
+            const countFramesHandle = await page.evaluateHandle(`${countFrames}`);
+            const participant1Samples = await participant1
+                .getByTestId("remoteParticipant")
+                .evaluate((element: HTMLVideoElement, countFramesFn) => {
+                    const countFrames = countFramesFn as CountFramesFunction;
+                    // counts 15 samples in a 1000ms interval.
+                    return countFrames(element, 1000, 15);
+                }, countFramesHandle);
+            const participant1FrameStatistics = makeFrameStatistics(participant1Samples);
+
+            expect(participant1FrameStatistics.minFps).toBeGreaterThan(1);
         });
     });
 });
