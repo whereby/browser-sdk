aa4623a5f2338c966bb4db9199bfc872ae901c8e
Kevin Hanna <kevin.hanna@whereby.com>
2023-10-18 09:32:28 +0100

    remove build step for npm distribution
    we can expect anyone installing from npm is using a bundler, so bundling
    ourselves is redundant and causing build issues intermittently.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
     preset: "ts-jest",
     testEnvironment: "jsdom",
     testMatch: ["<rootDir>/src/**/__tests__/**/*.[jt]s?(x)", "<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)"],
