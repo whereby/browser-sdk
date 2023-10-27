526dbaedca368631fa5465770527c12c85c3eb23
Thomas Frivold <thomas.frivold@whereby.com>
2023-10-27 09:39:01 +0100

    Add store

---
+++ src/lib/redux/listenerMiddleware.ts
@@ -0,0 +1,13 @@
+import { createListenerMiddleware, addListener } from "@reduxjs/toolkit";
+import type { TypedStartListening, TypedAddListener } from "@reduxjs/toolkit";
+
+import type { RootState, AppDispatch } from "./store";
+import { createServices } from "../services";
+
+export const listenerMiddleware = createListenerMiddleware();
+
+export type AppStartListening = TypedStartListening<RootState, AppDispatch, ReturnType<typeof createServices>>;
+
+export const startAppListening = listenerMiddleware.startListening as AppStartListening;
+
+export const addAppListener = addListener as TypedAddListener<RootState, AppDispatch>;
