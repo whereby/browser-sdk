316b8ccaa541016ecc059588024bc7f6e917376a
Håvard Holvik <havard@whereby.com>
2023-11-08 10:24:38 +0100

    Update node version + replace process envs
    Align node version with what we are using elsewhere in the company.
    Also replace process.env variables with their actual values to prevent
    setups such as Vite needing more configuration.

--- rollup.config.js
+++ rollup.config.js
@@ -29,17 +29,29 @@ const replaceValues = {
         "process.env.AWF_BASE_URL": JSON.stringify(process.env.AWF_BASE_URL),
         "process.env.AWF_API_BASE_URL": JSON.stringify(process.env.AWF_API_BASE_URL),
         "process.env.AP_ROOM_BASE_URL": JSON.stringify(process.env.AP_ROOM_BASE_URL),
+        "process.env.RTCSTATS_URL": JSON.stringify(process.env.RTCSTATS_URL || "wss://rtcstats.srv.whereby.com"),
+        "process.env.REACT_APP_API_BASE_URL": JSON.stringify(
+            process.env.REACT_APP_API_BASE_URL || "https://api.whereby.dev"
+        ),
+        "process.env.REACT_APP_SIGNAL_BASE_URL": JSON.stringify(
+            process.env.REACT_APP_SIGNAL_BASE_URL || "wss://signal.appearin.net"
+        ),
     },
 };
 
 const plugins = [
     replace(replaceValues),
     replace({
+        preventAssignment: true,
         // jslib-media uses global.navigator for some gUM calls, replace these
         delimiters: [" ", "."],
         values: { "global.navigator.mediaDevices": " navigator.mediaDevices." },
     }),
-    nodeResolve({ preferBuiltins: true, resolveOnly: [/@whereby\/jslib-media/] }),
+    nodeResolve({
+        // only include @whereby/jslib-media in our bundle
+        preferBuiltins: true,
+        resolveOnly: [/@whereby\/jslib-media/],
+    }),
     commonjs(),
     typescript(),
 ];
