aca4d6040492ed41fbefd012d7dd40e4741c738e
Thomas Frivold <thomas.frivold@whereby.com>
2023-11-20 19:33:08 +0000

    Add streaming slice

---
+++ src/lib/core/RoomConnection/redux/slices/streaming.ts
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+export interface StreamingState {
+    isStreaming: boolean;
+    error: unknown;
+    startedAt?: number;
+}
+
+const initialState: StreamingState = {
+    isStreaming: false,
+    error: null,
+    startedAt: undefined,
+};
+
+export const streamingSlice = createSlice({
+    name: "streaming",
+    initialState,
+    reducers: {
+        doHandleStreamingStarted: (state) => {
+            return {
+                ...state,
+                isStreaming: true,
+                error: null,
+                // We don't have the streaming start time stored on the
+                // server, so we use the current time instead. This gives
+                // an invalid timestamp for "Client B" if "Client A" has
+                // been streaming for a while before "Client B" joins.
+                startedAt: new Date().getTime(),
+            };
+        },
+        doHandleStreamingStopped: (state) => {
+            return {
+                ...state,
+                isStreaming: false,
+            };
+        },
+    },
+});
+
+export const { doHandleStreamingStarted, doHandleStreamingStopped } = streamingSlice.actions;
+
+export const selectStreamingRaw = (state: RootState) => state.streaming;
