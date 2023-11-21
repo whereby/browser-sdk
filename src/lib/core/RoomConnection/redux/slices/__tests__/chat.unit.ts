cdaadd65600dac58c36c1e9dc500c22f2c864d55
Thomas Frivold <thomas.frivold@whereby.com>
2023-11-21 19:31:05 +0000

    Add chat slice tests

---
+++ src/lib/core/RoomConnection/redux/slices/__tests__/chat.unit.ts
@@ -0,0 +1,24 @@
+import { chatSlice } from "../chat";
+
+describe("chatSlice", () => {
+    describe("reducers", () => {
+        it("doChatMessageReceived", () => {
+            const state = chatSlice.reducer(
+                undefined,
+                chatSlice.actions.doChatMessageReceived({
+                    senderId: "senderId",
+                    timestamp: "123",
+                    text: "text",
+                })
+            );
+
+            expect(state.chatMessages).toEqual([
+                {
+                    senderId: "senderId",
+                    timestamp: "123",
+                    text: "text",
+                },
+            ]);
+        });
+    });
+});
---
+++ src/lib/core/RoomConnection/redux/tests/store/chat.spec.ts
@@ -0,0 +1,12 @@
+import { createStore, mockSignalEmit } from "../store.setup";
+import { doSendChatMessage } from "../../slices/chat";
+
+describe("doSendChatMessage", () => {
+    it("should emit chat_message", async () => {
+        const store = createStore({ withSignalConnection: true });
+
+        await store.dispatch(doSendChatMessage({ text: "text" }));
+
+        expect(mockSignalEmit).toHaveBeenCalledWith("chat_message", { text: "text" });
+    });
+});
