d99c13a11025b7993681b0bc0c1de14359378d0d
Thomas Frivold <thomas.frivold@whereby.com>
2023-11-23 15:10:48 +0000

    Refactor remote participant slice

--- src/lib/__tests__/RoomConnection.spec.ts
+++ src/lib/__tests__/RoomConnection.spec.ts
@@ -44,21 +44,19 @@ describe("RoomConnection", () => {
 });
 
 describe("handleStreamAdded", () => {
-    let remoteParticipants: RemoteParticipant[];
-
-    beforeEach(() => {
-        remoteParticipants = [
-            new RemoteParticipant({
-                displayName: "Participant",
-                id: "id",
-                newJoiner: false,
-                streams: ["0", "screenshare"],
-                isAudioEnabled: true,
-                isVideoEnabled: true,
-            }),
-        ];
-    });
-
+    // let remoteParticipants: RemoteParticipant[];
+    // beforeEach(() => {
+    //     remoteParticipants = [
+    //         new RemoteParticipant({
+    //             displayName: "Participant",
+    //             id: "id",
+    //             newJoiner: false,
+    //             streams: ["0", "screenshare"],
+    //             isAudioEnabled: true,
+    //             isVideoEnabled: true,
+    //         }),
+    //     ];
+    // });
     // it("should return undefined if remote participant cannot be found", () => {
     //     const res = handleStreamAdded(remoteParticipants, {
     //         clientId: "zzz",
@@ -66,38 +64,31 @@ describe("handleStreamAdded", () => {
     //         streamId: undefined,
     //         streamType: undefined,
     //     });
-
     //     expect(res).toEqual(undefined);
     // });
-
     // it("should return `participant_stream_added` when stream id cannot be matched", () => {
     //     const clientId = "id";
     //     const stream = new MockMediaStream();
     //     const streamId = undefined;
-
     //     const res = handleStreamAdded(remoteParticipants, {
     //         clientId,
     //         stream,
     //         streamId,
     //         streamType: undefined,
     //     });
-
     //     expect(res?.type).toEqual("participant_stream_added");
     //     expect(res?.detail).toEqual({ participantId: clientId, stream, streamId: stream.id });
     // });
-
     // it("should return `screenshare_started` when stream id is matched", () => {
     //     const clientId = "id";
     //     const stream = new MockMediaStream();
     //     const streamId = "screenshare";
-
     //     const res = handleStreamAdded(remoteParticipants, {
     //         clientId,
     //         stream,
     //         streamId,
     //         streamType: undefined,
     //     });
-
     //     expect(res?.type).toEqual("screenshare_started");
     //     expect(res?.detail).toEqual({
     //         participantId: clientId,
