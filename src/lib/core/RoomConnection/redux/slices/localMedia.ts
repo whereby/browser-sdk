84b6fb583ac7a4e8c4e3c06c11dbcd287e4aa68f
Håvard Holvik <havard@whereby.com>
2023-11-24 10:52:53 +0100

    "Fix" local local media
    Introduce new slice for local media which is responsible for starting
    the local stream. Ensure the other slices takes this state into account
    when eg joining a room.

---
+++ src/lib/core/RoomConnection/redux/slices/localMedia.ts
@@ -0,0 +1,88 @@
+import { createSlice, createSelector } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+import { createAppThunk } from "../asyncThunk";
+import LocalMedia from "~/lib/LocalMedia";
+import { doAppJoin } from "./app";
+import { createReactor } from "../listenerMiddleware";
+
+/**
+ * State & reducer
+ */
+export interface LocalMediaState {
+    isStarting: boolean;
+    hasStarted: boolean;
+    localMediaInstance?: LocalMedia;
+}
+
+const initialState: LocalMediaState = {
+    isStarting: false,
+    hasStarted: false,
+    localMediaInstance: undefined,
+};
+
+export const localMediaSlice = createSlice({
+    name: "localMedia",
+    initialState,
+    reducers: {
+        starting: (state) => {
+            return {
+                ...state,
+                hasStarted: false,
+                isStarting: true,
+            };
+        },
+        started: (state) => {
+            return {
+                ...state,
+                hasStarted: true,
+                isStarting: false,
+            };
+        },
+    },
+    extraReducers(builder) {
+        builder.addCase(doAppJoin, (state, action) => {
+            return {
+                ...state,
+                localMediaInstance: action.payload.localMedia,
+            };
+        });
+    },
+});
+
+/**
+ * Action creators
+ */
+export const { started, starting } = localMediaSlice.actions;
+
+export const doLocalMediaStart = createAppThunk(() => async (dispatch, getState) => {
+    const { localMediaInstance } = selectLocalMediaRaw(getState());
+    if (!localMediaInstance) {
+        return;
+    }
+
+    dispatch(starting());
+    await localMediaInstance.start();
+    dispatch(started());
+});
+
+/**
+ * Selectors
+ */
+export const selectLocalMediaRaw = (state: RootState) => state.localMedia;
+export const selectLocalMediaInstance = (state: RootState) => state.localMedia.localMediaInstance;
+export const selectLocalMediaStream = (state: RootState) => state.localMedia.localMediaInstance?.stream;
+export const selectLocalMediaStarted = (state: RootState) => state.localMedia.hasStarted;
+
+export const selectLocalMediaShouldStart = createSelector([selectLocalMediaRaw], (raw) => {
+    const { hasStarted, isStarting, localMediaInstance } = raw;
+    return !!localMediaInstance && !hasStarted && !isStarting;
+});
+
+/**
+ * Reactors
+ */
+createReactor((_, { dispatch, getState }) => {
+    if (selectLocalMediaShouldStart(getState())) {
+        dispatch(doLocalMediaStart());
+    }
+});
