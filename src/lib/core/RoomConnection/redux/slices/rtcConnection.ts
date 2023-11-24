d98ab35b3af848604468d82050d3e8ddc59988e7
Håvard Holvik <havard@whereby.com>
2023-11-24 13:23:45 +0100

    Fix not adding local stream
    Fixes the issue where we did not add local stream to rtc manager.
    Also fixes an issue wherre the room connection slice tried to start
    local media as well, causing ended tracks.

--- src/lib/core/RoomConnection/redux/slices/rtcConnection.ts
+++ src/lib/core/RoomConnection/redux/slices/rtcConnection.ts
@@ -1,6 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { AppDispatch, RootState } from "../store";
-import { createAppAsyncThunk, createAppThunk } from "../asyncThunk";
+import { AnyAction, createSlice, PayloadAction, ThunkDispatch } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+import { createAppThunk } from "../asyncThunk";
 import RtcManager from "@whereby/jslib-media/src/webrtc/RtcManager";
 import { selectSignalConnectionRaw } from "./signalConnection";
 import RtcManagerDispatcher, {
@@ -11,27 +11,9 @@ import RtcManagerDispatcher, {
 import { createReactor } from "../listenerMiddleware";
 import { participantStreamAdded, selectRemoteParticipants, streamStatusUpdated } from "./remoteParticipants";
 import { StreamState } from "~/lib/RoomParticipant";
-import { selectLocalMediaInstance } from "./localMedia";
+import { selectLocalMediaInstance, selectLocalMediaStarted } from "./localMedia";
 
-export interface RtcConnectionState {
-    error: unknown;
-    status: "" | "ready" | "reconnect";
-    isCreatingDispatcher: boolean;
-    dispatcherCreated: boolean;
-    rtcManagerDispatcher: RtcManagerDispatcher | null;
-    rtcManager: RtcManager | null;
-}
-
-const initialState: RtcConnectionState = {
-    error: null,
-    status: "",
-    isCreatingDispatcher: false,
-    dispatcherCreated: false,
-    rtcManagerDispatcher: null,
-    rtcManager: null,
-};
-
-export const createWebRtcEmitter = (dispatch: AppDispatch) => {
+export const createWebRtcEmitter = (dispatch: ThunkDispatch<RootState, unknown, AnyAction>) => {
     return {
         emit: (eventName: keyof RtcEvents, data: RtcEvents[keyof RtcEvents]) => {
             if (eventName === "rtc_manager_created") {
@@ -39,7 +21,7 @@ export const createWebRtcEmitter = (dispatch: AppDispatch) => {
             } else if (eventName === "stream_added") {
                 dispatch(doStreamAdded(data as RtcStreamAddedPayload));
             } else if (eventName === "rtc_manager_destroyed") {
-                dispatch(doRtcManagerDestroyed());
+                dispatch(rtcManagerDestroyed());
             } else {
                 //console.log(`Unhandled RTC event ${eventName}`);
             }
@@ -47,63 +29,115 @@ export const createWebRtcEmitter = (dispatch: AppDispatch) => {
     };
 };
 
-export const doRtcManagerCreated = createAppAsyncThunk(
-    "rtcConnection/doRtcManagerCreated",
-    async (payload: RtcManagerCreatedPayload, { getState }) => {
-        const { rtcManager } = payload;
-        const localMedia = selectLocalMediaInstance(getState());
+/**
+ * Reducer
+ */
+export interface RtcConnectionState {
+    dispatcherCreated: boolean;
+    error: unknown;
+    isCreatingDispatcher: boolean;
+    rtcManager: RtcManager | null;
+    rtcManagerDispatcher: RtcManagerDispatcher | null;
+    rtcManagerInitialized: boolean;
+    status: "" | "ready" | "reconnect";
+}
 
-        localMedia?.addRtcManager(rtcManager);
-
-        if (localMedia?.stream) {
-            rtcManager.addNewStream(
-                "0",
-                localMedia.stream,
-                !localMedia.isMicrophoneEnabled(),
-                !localMedia.isCameraEnabled()
-            );
-        }
-
-        return rtcManager;
-    }
-);
-
-export const doStreamAdded = createAppAsyncThunk(
-    "rtcConnection/doStreamAdded",
-    async (payload: RtcStreamAddedPayload, { dispatch, getState, extra }) => {
-        const state = getState();
-        const remoteParticipants = selectRemoteParticipants(state);
-
-        const { clientId, stream, streamId, streamType } = payload;
-
-        if (!streamId) {
-            payload.streamId = stream.id;
-        }
-
-        const remoteParticipant = remoteParticipants.find((p) => p.id === clientId);
-        if (!remoteParticipant) {
-            return;
-        }
-
-        const remoteParticipantStream =
-            remoteParticipant.streams.find((s) => s.id === streamId) || remoteParticipant.streams[0];
-
-        if (
-            (remoteParticipant.stream && remoteParticipant.stream.id === streamId) ||
-            (!remoteParticipant.stream && streamType === "webcam") ||
-            (!remoteParticipant.stream && !streamType && remoteParticipant.streams.indexOf(remoteParticipantStream) < 1)
-        ) {
-            dispatch(participantStreamAdded({ clientId, streamId: stream.id, stream, streamType }));
-        }
-        // update remote participant screen share stream
-    }
-);
+const initialState: RtcConnectionState = {
+    dispatcherCreated: false,
+    error: null,
+    isCreatingDispatcher: false,
+    rtcManager: null,
+    rtcManagerDispatcher: null,
+    rtcManagerInitialized: false,
+    status: "",
+};
 
 export interface StreamStatusUpdate {
     clientId: string;
     streamId: string;
     state: StreamState;
 }
+
+export const rtcConnectionSlice = createSlice({
+    name: "rtcConnection",
+    initialState,
+    reducers: {
+        rtcDispatcherCreated: (state, action: PayloadAction<RtcManagerDispatcher>) => {
+            return {
+                ...state,
+                dispatcherCreated: true,
+                rtcManagerDispatcher: action.payload,
+            };
+        },
+        rtcManagerCreated: (state, action: PayloadAction<RtcManager>) => {
+            return {
+                ...state,
+                rtcManager: action.payload,
+                status: "ready",
+            };
+        },
+        rtcManagerDestroyed: (state) => {
+            return {
+                ...state,
+                dispatcherCreated: false,
+                rtcManagerDispatcher: null,
+                rtcManager: null,
+                rtcManagerInitialized: false,
+            };
+        },
+        rtcManagerInitialized: (state) => {
+            return {
+                ...state,
+                rtcManagerInitialized: true,
+            };
+        },
+    },
+});
+
+/**
+ * Action creators
+ */
+export const { rtcDispatcherCreated, rtcManagerCreated, rtcManagerDestroyed, rtcManagerInitialized } =
+    rtcConnectionSlice.actions;
+
+export const doConnectRtc = createAppThunk(() => (dispatch, getState) => {
+    const state = getState();
+    const socket = selectSignalConnectionRaw(state).socket;
+    const dispatcher = selectRtcConnectionRaw(state).rtcManagerDispatcher;
+    const localMedia = selectLocalMediaInstance(state);
+
+    if (dispatcher) {
+        return;
+    }
+
+    const webrtcProvider = {
+        getMediaConstraints: () => ({
+            audio: localMedia?.isMicrophoneEnabled() || false,
+            video: localMedia?.isCameraEnabled() || false,
+        }),
+        deferrable(clientId: string) {
+            return !clientId;
+        },
+    };
+
+    const rtcManagerDispatcher = new RtcManagerDispatcher({
+        emitter: createWebRtcEmitter(dispatch),
+        serverSocket: socket,
+        logger: console,
+        webrtcProvider,
+        features: {
+            lowDataModeEnabled: false,
+            sfuServerOverrideHost: undefined,
+            turnServerOverrideHost: undefined,
+            useOnlyTURN: undefined,
+            vp9On: false,
+            h264On: false,
+            simulcastScreenshareOn: false,
+        },
+    });
+
+    dispatch(rtcDispatcherCreated(rtcManagerDispatcher));
+});
 
 const doHandleAcceptStreams = createAppThunk((payload: StreamStatusUpdate[]) => (dispatch, getState) => {
     const state = getState();
@@ -145,101 +179,86 @@ const doHandleAcceptStreams = createAppThunk((payload: StreamStatusUpdate[]) => 
             // done_accept
         }
         updates.push({ clientId, streamId, state: state.replace(/to_|new_|old_/, "done_") as StreamState });
-        //participant.updateStreamState(streamId, state.replace(/to_|new_|old_/, "done_") as StreamState);
     }
 
     dispatch(streamStatusUpdated(updates));
 });
 
-export const doConnectRtc = createAppAsyncThunk(
-    "rtcConnection/doConnectRtc",
-    async (payload, { dispatch, getState }) => {
-        const state = getState();
-        const socket = selectSignalConnectionRaw(state).socket;
-        const dispatcher = selectRtcConnectionRaw(state).rtcManagerDispatcher;
-        const localMedia = selectLocalMediaInstance(state);
+export const doStreamAdded = createAppThunk((payload: RtcStreamAddedPayload) => (dispatch, getState) => {
+    const state = getState();
+    const remoteParticipants = selectRemoteParticipants(state);
 
-        if (dispatcher) {
-            throw new Error("RTC dispatcher already exists");
-        }
+    const { clientId, stream, streamId, streamType } = payload;
 
-        const webrtcProvider = {
-            getMediaConstraints: () => ({
-                audio: localMedia?.isMicrophoneEnabled() || false,
-                video: localMedia?.isCameraEnabled() || false,
-            }),
-            deferrable(clientId: string) {
-                return !clientId;
-            },
-        };
-
-        const rtcManagerDispatcher = new RtcManagerDispatcher({
-            emitter: createWebRtcEmitter(dispatch),
-            serverSocket: socket,
-            logger: console,
-            webrtcProvider,
-            features: {
-                lowDataModeEnabled: false,
-                sfuServerOverrideHost: undefined,
-                turnServerOverrideHost: undefined,
-                useOnlyTURN: undefined,
-                vp9On: false,
-                h264On: false,
-                simulcastScreenshareOn: false,
-            },
-        });
-
-        return rtcManagerDispatcher;
+    if (!streamId) {
+        payload.streamId = stream.id;
     }
-);
 
-export const rtcConnectionSlice = createSlice({
-    name: "rtcConnection",
-    initialState,
-    reducers: {
-        doRtcManagerDestroyed: (state) => {
-            return {
-                ...state,
-                dispatcherCreated: false,
-                rtcManagerDispatcher: null,
-                rtcManager: null,
-            };
-        },
-    },
-    extraReducers: (builder) => {
-        builder.addCase(doConnectRtc.pending, (state) => {
-            return {
-                ...state,
-                isCreatingDispatcher: true,
-            };
-        });
-        builder.addCase(doConnectRtc.fulfilled, (state, action) => {
-            return {
-                ...state,
-                dispatcherCreated: true,
-                rtcManagerDispatcher: action.payload,
-            };
-        });
-        builder.addCase(doRtcManagerCreated.fulfilled, (state, action) => {
-            return {
-                ...state,
-                rtcManager: action.payload,
-                status: "ready",
-            };
-        });
-    },
+    const remoteParticipant = remoteParticipants.find((p) => p.id === clientId);
+    if (!remoteParticipant) {
+        return;
+    }
+
+    const remoteParticipantStream =
+        remoteParticipant.streams.find((s) => s.id === streamId) || remoteParticipant.streams[0];
+
+    if (
+        (remoteParticipant.stream && remoteParticipant.stream.id === streamId) ||
+        (!remoteParticipant.stream && streamType === "webcam") ||
+        (!remoteParticipant.stream && !streamType && remoteParticipant.streams.indexOf(remoteParticipantStream) < 1)
+    ) {
+        dispatch(participantStreamAdded({ clientId, streamId: stream.id, stream, streamType }));
+    }
 });
 
-export const { doRtcManagerDestroyed } = rtcConnectionSlice.actions;
+export const doRtcManagerCreated = createAppThunk((payload: RtcManagerCreatedPayload) => (dispatch, getState) => {
+    const { rtcManager } = payload;
+    const localMedia = selectLocalMediaInstance(getState());
 
+    localMedia?.addRtcManager(rtcManager);
+
+    dispatch(rtcManagerCreated(rtcManager));
+});
+
+export const doRtcManagerInitialize = createAppThunk(() => (dispatch, getState) => {
+    const localMedia = selectLocalMediaInstance(getState());
+    const rtcManager = selectRtcConnectionRaw(getState()).rtcManager;
+
+    if (localMedia?.stream && rtcManager) {
+        rtcManager.addNewStream(
+            "0",
+            localMedia.stream,
+            !localMedia.isMicrophoneEnabled(),
+            !localMedia.isCameraEnabled()
+        );
+    }
+
+    dispatch(rtcManagerInitialized());
+});
+
+/**
+ * Selectors
+ */
 export const selectRtcConnectionRaw = (state: RootState) => state.rtcConnection;
 
+/**
+ * Reactors
+ */
 createReactor((_, { dispatch, getState }) => {
     const rtcConnection = selectRtcConnectionRaw(getState());
     const signalConnection = selectSignalConnectionRaw(getState());
 
     if (!rtcConnection.dispatcherCreated && !rtcConnection.isCreatingDispatcher && signalConnection.socket) {
         dispatch(doConnectRtc());
+    }
+});
+
+createReactor((_, { dispatch, getState }) => {
+    const { rtcManager, rtcManagerInitialized } = selectRtcConnectionRaw(getState());
+    const localMediaStarted = selectLocalMediaStarted(getState());
+
+    if (localMediaStarted && rtcManager && !rtcManagerInitialized) {
+        dispatch(doRtcManagerInitialize());
     }
 });
 
