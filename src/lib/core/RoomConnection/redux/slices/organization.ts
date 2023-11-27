ae2504cc871354dce8b8e5844d0bdb70859ab3cd
Thomas Frivold <thomas.frivold@whereby.com>
2023-11-27 15:30:15 +0000

    Only run reactors when dependencies change

--- src/lib/core/RoomConnection/redux/slices/organization.ts
+++ src/lib/core/RoomConnection/redux/slices/organization.ts
@@ -80,18 +80,21 @@ export const selectOrganizationId = (state: RootState) => state.organization.dat
  * Reducers
  */
 
-createReactor((_, { dispatch, getState }) => {
-    const wantsToJoin = selectAppWantsToJoin(getState());
-    const organization = selectOrganizationRaw(getState());
-    const deviceCredentials = selectDeviceCredentialsRaw(getState());
+createReactor(
+    [selectAppWantsToJoin, selectOrganizationRaw, selectDeviceCredentialsRaw],
+    (_, { dispatch, getState }) => {
+        const wantsToJoin = selectAppWantsToJoin(getState());
+        const organization = selectOrganizationRaw(getState());
+        const deviceCredentials = selectDeviceCredentialsRaw(getState());
 
-    if (
-        wantsToJoin &&
-        !organization.data &&
-        !organization.isFetching &&
-        !organization.error &&
-        !deviceCredentials.isFetching
-    ) {
-        dispatch(doOrganizationFetch());
+        if (
+            wantsToJoin &&
+            !organization.data &&
+            !organization.isFetching &&
+            !organization.error &&
+            !deviceCredentials.isFetching
+        ) {
+            dispatch(doOrganizationFetch());
+        }
     }
-});
+);
