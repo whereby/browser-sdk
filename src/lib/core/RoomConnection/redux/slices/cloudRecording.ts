5b6720fe9175276ab9f658cf3328c4174840f641
Håvard Holvik <havard@whereby.com>
2023-11-28 11:01:54 +0100

    Cleanup

--- src/lib/core/RoomConnection/redux/slices/cloudRecording.ts
+++ src/lib/core/RoomConnection/redux/slices/cloudRecording.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../../redux/store";
-import { createAppThunk } from "../../../redux/asyncThunk";
+import { createAppThunk } from "../../../redux/thunk";
 import { selectSignalConnectionRaw, signalEvents } from "./signalConnection";
 
 /**
