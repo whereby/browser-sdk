fa073ddae94fd2b84c9aa63d2cd6f21e5cbbdf62
Håvard Holvik <havard@whereby.com>
2023-12-01 12:14:07 +0100

    Re-add type export LocalMEdiaResult

--- src/lib/react/index.ts
+++ src/lib/react/index.ts
@@ -2,6 +2,8 @@ export { default as VideoView } from "./VideoView";
 export { useRoomConnection } from "./useRoomConnection";
 export { default as useReduxLocalMedia } from "./useLocalMedia";
 
+export type { LocalMediaRef as UseLocalMediaResult } from "./useLocalMedia";
+
 export type {
     ChatMessageState as ChatMessage,
     LocalParticipantState as LocalParticipant,
