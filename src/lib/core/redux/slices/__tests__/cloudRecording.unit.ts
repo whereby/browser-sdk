0bc64b6c7f7c9abffb816601a3404b27a5f32796
Thomas Frivold <thomas.frivold@whereby.com>
2023-12-04 11:48:28 +0000

    Fix unit tests for cloud recording

--- src/lib/core/redux/slices/__tests__/cloudRecording.unit.ts
+++ src/lib/core/redux/slices/__tests__/cloudRecording.unit.ts
@@ -1,17 +1,52 @@
 import { cloudRecordingSlice } from "../cloudRecording";
+import { signalEvents } from "../signalConnection/actions";
 
 describe("cloudRecordingSlice", () => {
     describe("reducers", () => {
-        // it("doHandleCloudRecordingStopped", () => {
-        //     const state = cloudRecordingSlice.reducer(
-        //         undefined,
-        //         cloudRecordingSlice.actions.doHandleCloudRecordingStopped()
-        //     );
-        //     expect(state).toEqual({
-        //         error: null,
-        //         isRecording: false,
-        //         startedAt: undefined,
-        //     });
-        // });
+        // We only handle error in this event. We start cloud recording when a new recorder client joins.
+        it("signalEvents.cloudRecordingStarted", () => {
+            const state = cloudRecordingSlice.reducer(
+                undefined,
+                signalEvents.cloudRecordingStarted({ error: "some error" })
+            );
+            expect(state).toEqual({
+                error: "some error",
+                isRecording: false,
+                status: "error",
+            });
+        });
+
+        it("signalEvents.cloudRecordingStopped", () => {
+            const state = cloudRecordingSlice.reducer(undefined, signalEvents.cloudRecordingStopped());
+            expect(state).toEqual({
+                error: null,
+                isRecording: false,
+            });
+        });
+
+        it("signalEvents.newClient", () => {
+            const state = cloudRecordingSlice.reducer(
+                undefined,
+                signalEvents.newClient({
+                    client: {
+                        displayName: "recorder",
+                        streams: [],
+                        isAudioEnabled: true,
+                        isVideoEnabled: false,
+                        id: "id",
+                        role: {
+                            roleName: "recorder",
+                        },
+                        startedCloudRecordingAt: "2021-01-01T00:00:00.000Z",
+                    },
+                })
+            );
+            expect(state).toEqual({
+                error: null,
+                isRecording: true,
+                status: "recording",
+                startedAt: 1609459200000,
+            });
+        });
     });
 });
