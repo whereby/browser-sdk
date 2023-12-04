7f0897616e247ce9985868610ee151e46e8d99b2
Håvard Holvik <havard@whereby.com>
2023-12-04 15:17:07 +0100

    Update waiting participants on room join
    There was a bug preventing the host from seeing waiting participants
    when joining after them.

---
+++ src/lib/core/redux/slices/__tests__/waitingParticipants.unit.ts
@@ -0,0 +1,34 @@
+import { waitingParticipantsSlice } from "../waitingParticipants";
+import { signalEvents } from "../signalConnection/actions";
+
+describe("reducer", () => {
+    describe('on signal "room_joined"', () => {
+        it("should update waitingParticipants array based on knockers in the room", () => {
+            const clientId = "client-id";
+            const displayName = "Client";
+
+            const state = waitingParticipantsSlice.reducer(
+                undefined,
+                signalEvents.roomJoined({
+                    isLocked: true,
+                    selfId: "self-id",
+                    room: {
+                        clients: [],
+                        knockers: [
+                            {
+                                clientId,
+                                displayName,
+                                imageUrl: null,
+                                liveVideo: false,
+                                userAvatarUrl: null,
+                                userId: null,
+                            },
+                        ],
+                    },
+                })
+            );
+
+            expect(state).toEqual({ waitingParticipants: [{ id: clientId, displayName }] });
+        });
+    });
+});
