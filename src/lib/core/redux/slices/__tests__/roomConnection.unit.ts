29938d1509f9cbe5b65b0f879b2ac43c65abbf86
Thomas Frivold <thomas.frivold@whereby.com>
2023-12-04 15:01:36 +0000

    Add unit tests for room connection slice

---
+++ src/lib/core/redux/slices/__tests__/roomConnection.unit.ts
@@ -0,0 +1,68 @@
+import { oneOf } from "../../../../../lib/__mocks__/appMocks";
+import { roomConnectionSlice, selectShouldConnectRoom } from "../roomConnection";
+import { signalEvents } from "../signalConnection/actions";
+
+describe("roomConnectionSlice", () => {
+    describe("reducers", () => {
+        describe("signalEvents.roomJoined", () => {
+            it("should set status to room_locked if the room is locked", () => {
+                const result = roomConnectionSlice.reducer(
+                    undefined,
+                    signalEvents.roomJoined({
+                        error: "room_locked",
+                        selfId: "selfId",
+                        isLocked: true,
+                    })
+                );
+
+                expect(result).toEqual({
+                    status: "room_locked",
+                    error: null,
+                });
+            });
+
+            it("should set status to connected if the room is not locked", () => {
+                const result = roomConnectionSlice.reducer(
+                    undefined,
+                    signalEvents.roomJoined({
+                        error: "room_locked",
+                        selfId: "selfId",
+                        isLocked: false,
+                    })
+                );
+
+                expect(result).toEqual({
+                    status: "connected",
+                    error: null,
+                });
+            });
+        });
+    });
+
+    describe("reactors", () => {
+        describe("selectShouldConnectRoom", () => {
+            const x = () => oneOf(true, false);
+
+            it.each`
+                organizationId | roomConnectionStatus | signalIdentified | localMediaStatus | expected
+                ${undefined}   | ${"initializing"}    | ${x()}           | ${"started"}     | ${false}
+                ${"orgId"}     | ${"initializing"}    | ${true}          | ${"started"}     | ${true}
+                ${"orgId"}     | ${"connected"}       | ${x()}           | ${"started"}     | ${false}
+                ${"orgId"}     | ${"initializing"}    | ${false}         | ${"starting"}    | ${false}
+                ${"orgId"}     | ${"initializing"}    | ${x()}           | ${"error"}       | ${false}
+            `(
+                "should return $expected when hasOrganizationIdFetched=$hasOrganizationIdFetched, roomConnectionStatus=$roomConnectionStatus, signalIdentified=$signalIdentified, localMediaStatus=$localMediaStatus",
+                ({ organizationId, roomConnectionStatus, signalIdentified, localMediaStatus, expected }) => {
+                    expect(
+                        selectShouldConnectRoom.resultFunc(
+                            organizationId,
+                            roomConnectionStatus,
+                            signalIdentified,
+                            localMediaStatus
+                        )
+                    ).toEqual(expected);
+                }
+            );
+        });
+    });
+});
