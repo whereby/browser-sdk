3ee35465a790e3ecd85cd943c07c1ee13088e884
Håvard Holvik <havard@whereby.com>
2023-12-05 10:37:24 +0100

    Only run tests with spec | test | unit

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "jsdom",
-    testMatch: ["<rootDir>/src/**/__tests__/**/*.[jt]s?(x)", "<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)"],
+    testMatch: ["<rootDir>/src/**/?(*.)+(spec|test|unit).[jt]s?(x)"],
     transform: {
         "^.+\\.(j|t)sx?$": "ts-jest",
     },
