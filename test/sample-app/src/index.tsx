2e63e20669fd5f7ce8ec93c775a9c4e447c72e3f
Håvard Holvik <havard@whereby.com>
2023-12-05 14:12:26 +0100

    Remove strict mode in sample app

--- test/sample-app/src/index.tsx
+++ test/sample-app/src/index.tsx
@@ -4,8 +4,5 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+
+root.render(<App />);
