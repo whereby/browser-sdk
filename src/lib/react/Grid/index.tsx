948e2e1b165ec52732f7421ff9f48d4646cf2c46
Thomas Frivold <thomas.frivold@whereby.com>
2024-01-16 09:35:27 +0100

    Report aspect ratio of each video

--- src/lib/react/Grid/index.tsx
+++ src/lib/react/Grid/index.tsx
@@ -10,10 +10,12 @@ function GridVideoCellView({
     cell,
     participant,
     render,
+    onSetAspectRatio,
 }: {
     cell: { clientId: string; bounds: { width: number; height: number }; origin: { top: number; left: number } };
     participant: RemoteParticipant | LocalParticipant;
     render?: () => React.ReactNode;
+    onSetAspectRatio: ({ aspectRatio }: { aspectRatio: number }) => void;
 }) {
     return (
         <div
@@ -26,7 +28,11 @@ function GridVideoCellView({
                 left: cell.origin.left,
             }}
         >
-            {render ? render() : participant.stream ? <VideoView style={{}} stream={participant.stream} /> : null}
+            {render ? (
+                render()
+            ) : participant.stream ? (
+                <VideoView style={{}} stream={participant.stream} onSetAspectRatio={onSetAspectRatio} />
+            ) : null}
         </div>
     );
 }
@@ -48,6 +54,7 @@ function Grid({ roomConnection, renderParticipant, videoGridGap = 0 }: GridProps
     const gridRef = React.useRef<HTMLDivElement>(null);
     const [videos, setVideos] = React.useState<ReturnType<typeof makeVideoCellView>[]>([]);
     const [stageLayout, setStageLayout] = React.useState<ReturnType<typeof calculateLayout> | null>(null);
+    const [aspectRatios, setAspectRatios] = React.useState<{ clientId: string; aspectRatio: number }[]>([]);
 
     React.useEffect(() => {
         setVideos(
@@ -61,6 +68,20 @@ function Grid({ roomConnection, renderParticipant, videoGridGap = 0 }: GridProps
             )
         );
     }, [remoteParticipants, localParticipant]);
+
+    React.useEffect(() => {
+        setVideos((prev) => {
+            return prev.map((video) => {
+                const aspectRatio = aspectRatios.find((item) => item.clientId === video.clientId)?.aspectRatio;
+
+                if (aspectRatio) {
+                    return { ...video, aspectRatio };
+                }
+
+                return video;
+            });
+        });
+    }, [aspectRatios]);
 
     React.useEffect(() => {
         if (!gridRef.current || !videos.length) {
@@ -109,7 +130,7 @@ function Grid({ roomConnection, renderParticipant, videoGridGap = 0 }: GridProps
             {[localParticipant, ...remoteParticipants].map((participant, i) => {
                 const cell = stageLayout?.videoGrid.cells[i];
 
-                if (!cell || !participant) return null;
+                if (!cell || !participant || !participant.stream || !cell.clientId) return null;
 
                 return (
                     <GridVideoCellView
@@ -117,6 +138,21 @@ function Grid({ roomConnection, renderParticipant, videoGridGap = 0 }: GridProps
                         cell={cell}
                         participant={participant}
                         render={renderParticipant ? () => renderParticipant({ cell, participant }) : undefined}
+                        onSetAspectRatio={({ aspectRatio }) => {
+                            setAspectRatios((prev) => {
+                                const index = prev.findIndex((item) => item.clientId === cell.clientId);
+
+                                if (index === -1) {
+                                    return [...prev, { clientId: cell.clientId, aspectRatio }];
+                                }
+
+                                return [
+                                    ...prev.slice(0, index),
+                                    { clientId: cell.clientId, aspectRatio },
+                                    ...prev.slice(index + 1),
+                                ];
+                            });
+                        }}
                     />
                 );
             })}
